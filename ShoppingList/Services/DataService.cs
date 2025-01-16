c7d30b801dfdb1e9746abf94c9f8d04cb444103d
TomaszWierzbinzzzki <tomaszwierzbinski486@gmail.com>
2025-01-16 09:50:43 +0100

    Update DataService.cs

--- ShoppingList/Services/DataService.cs
+++ ShoppingList/Services/DataService.cs
@@ -1,5 +1,5 @@
 ﻿using ShoppingList.Models;
-using Newtonsoft.Json; // Dodaj tę linię, aby korzystać z JsonConvert
+using Newtonsoft.Json;
 using System.IO;
 using System.Threading.Tasks;
 using System.Linq;
@@ -8,14 +8,11 @@ public class DataService
 {
     private string _filePath = Path.Combine(FileSystem.AppDataDirectory, "itemList.json");
 
-    // Funkcja zapisująca dane
     public async Task SaveDataAsync(ItemList itemList)
     {
         var jsonData = JsonConvert.SerializeObject(itemList);
         await File.WriteAllTextAsync(_filePath, jsonData);
     }
-
-    // Funkcja ładująca dane
     public async Task<ItemList> LoadDataAsync()
     {
         if (File.Exists(_filePath))
@@ -23,6 +20,6 @@ public class DataService
             var jsonData = await File.ReadAllTextAsync(_filePath);
             return JsonConvert.DeserializeObject<ItemList>(jsonData);
         }
-        return new ItemList(); // Jeśli plik nie istnieje, zwróć pustą listę
+        return new ItemList();
     }
 }
